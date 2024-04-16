9dd8af381a31a5d69374aeb9b89af767c5c6885d
saeed <saeedmouzarmi@gmail.com>
2024-04-16 17:10:17 +0330

    Fixed update command to work correctly.

--- app/Console/Commands/UpdatingRatesCommand.php
+++ app/Console/Commands/UpdatingRatesCommand.php
@@ -2,12 +2,14 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Currency;
 use App\Models\Rate;
 use App\Repositories\CurrencyRepository;
 use App\Repositories\RateRepository;
 use App\Services\CryptoService;
 use App\Services\Exchanger\FCSApi;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class UpdatingRatesCommand extends Command
 {
@@ -31,16 +33,25 @@ class UpdatingRatesCommand extends Command
     public function handle()
     {
         $rateRepository = new RateRepository(new Rate());
+        $currencyRepository = new CurrencyRepository(new Currency());
         $rates = $rateRepository->all();
-        $cryptoService = new CryptoService(new FCSApi());
+        $cryptoService = new CryptoService(new FCSApi(), $currencyRepository, $rateRepository);
         foreach ($rates as $rate) {
-            if (!$rate->internal) {
-                $firstSymbol = $rate->firstCurrency->symbol;
-                $secondSymbol = $rate->secondCurrency->symbol;
-                $ratio = $cryptoService->getRate($firstSymbol, $secondSymbol);
-                $rateRepository->updateRateRecord($rate->firstCurrency->id, $rate->secondCurrency->id, $ratio);
+            $firstSymbol = $rate->currency->symbol;
+            $secondSymbol = $rate->sourceCurrency->symbol;
+            try {
+                DB::beginTransaction();
+                $price = $cryptoService->getPrice($firstSymbol, $secondSymbol);
+                $this->info("Retrieved price for $firstSymbol/$secondSymbol: $price");
+                if ($price)
+
+                    $rateRepository->updateRateRecord($rate->currency->id, $rate->sourceCurrency->id, $price);
+                DB::commit();
+            } catch (\Exception $exception) {
+                DB::rollBack();
+
             }
         }
-
+        $rateRepository->updateRatesBasedOnOtherRates();
     }
 }
