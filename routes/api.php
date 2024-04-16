6537f09602c8bbe815bc915c0054eb3c52c103b5
saeed <saeedmouzarmi@gmail.com>
2024-04-16 17:08:29 +0330

    Modified route files.

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\CurrencyController;
+use App\Http\Controllers\Api\OrderController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::post("/orders", [\App\Http\Controllers\Api\OrderController::class, 'store']);
-Route::get("/orders", [\App\Http\Controllers\Api\OrderController::class, 'show']);
+Route::post("/orders", [OrderController::class, 'store']);
+Route::get("/orders", [OrderController::class, 'show']);
+Route::get("/currencies", [CurrencyController::class, 'index']);
+Route::get("/rates", [CurrencyController::class, 'rate']);
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,1 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-
-
-    $obj = new \App\Services\CryptoService(new \App\Services\Exchanger\FCSApi());
-    $a = $obj->getRate("BTC", "USD");
-    return response()->json($a);
-});
