b508c83b2424d6753ad3bd2106ea24433730bf0c
saeed <saeedmouzarmi@gmail.com>
2024-04-16 17:10:01 +0330

    Modified order service to run correctly.

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -6,47 +6,46 @@ use App\Models\Currency;
 use App\Repositories\CurrencyRepository;
 use App\Repositories\OrderRepository;
 use App\Repositories\RateRepository;
+use App\Services\Exchanger\CryptoExchangerInterface;
+use App\Services\Exchanger\FCSApi;
 use Illuminate\Support\Facades\DB;
 
 class OrderService
 {
     public function __construct(
         private OrderRepository    $orderRepository,
-        private RateRepository     $rateRepository,
         private CurrencyRepository $currencyRepository,
     )
     {
 
     }
 
-    public function createOrder(array $data): \Illuminate\Database\Eloquent\Model|false
+    public function createOrder(CryptoServiceInterface $cryptoService, array $data): \Illuminate\Database\Eloquent\Model|false
     {
-
-//        try {
-        DB::beginTransaction();
-        $firstCurrency = $this->currencyRepository->findBySymbol($data['first_symbol']);
-        $secondCurrency = $this->currencyRepository->findBySymbol($data['second_symbol']);
-        $email = $data['email'];
-        $amount = $data['amount'];
-
-        $rate = $this->rateRepository->getRateRecord($firstCurrency->id, $secondCurrency->id);
-        if ($rate->firstCurrency->id != $firstCurrency->id) $rate = 1 / $rate;
-        $amountReceived = $amount / $rate->ratio;
-        $payload = [
-            "rate_id" => $rate->id,
-            "amount_received" => $amountReceived,
-            "amount_paid" => $amount,
-            "rate_state_value" => $rate->ratio,
-            "email_address" => $email,
-        ];
-        $order = $this->orderRepository->create($payload);
-        $this->orderRepository->generateTrackingCode($order->id);
-        DB::commit();
-        return $order->fresh();
-//        } catch (\Exception $exception) {
-//            DB::rollBack();
-//            return false;
-//        }
+        try {
+            DB::beginTransaction();
+            $email = $data['email'];
+            $amount = $data['amount'];
+//            Cache storage for reading rates
+            $rate = $cryptoService->rateReader($data['first_symbol'], $data['second_symbol']);
+            $rateId = $rate['rate_id'];
+            $ratePrice = $rate['price'];
+            $amountReceived = $amount / $ratePrice;
+            $payload = [
+                "rate_id" => $rateId,
+                "amount_received" => $amountReceived,
+                "amount_paid" => $amount,
+                "rate_state_value" => $ratePrice,
+                "email_address" => $email,
+            ];
+            $order = $this->orderRepository->create($payload);
+            $this->orderRepository->generateTrackingCode($order->id);
+            DB::commit();
+            return $order->fresh();
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            return false;
+        }
 
     }
 }
